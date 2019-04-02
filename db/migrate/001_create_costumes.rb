6d331f97b6057ff75be5127234d2b66193ee81fd
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-02 22:27:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,5 +5,46 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.text :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+end
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.string :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string  :long_description
+    end
+  end
+end
