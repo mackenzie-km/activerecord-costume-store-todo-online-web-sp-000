f769631bc7a8bfcb25fd604a39960610eb7d4491
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-02 22:34:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -19,32 +19,4 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     end
   end
 end
-
-class CreateCostumeStores < ActiveRecord::Migration[4.2]
-  def change
-    create_table :costume_stores do |t|
-      t.string :name
-      t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
-    end
-  end
-end
-
-class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family_friendly
-      t.datetime :opening_date
-      t.datetime :closing_date
-      t.string  :description
-    end
-  end
 end
